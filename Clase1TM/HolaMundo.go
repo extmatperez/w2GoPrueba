a8d96cf55b75e6d17601836eb7a0cdd28fd3f6dc
Matias Perez <mataguper20@gmail.com>
2021-11-23 12:37:18 -0300

    Delete changes

--- Clase1TM/HolaMundo.go
+++ Clase1TM/HolaMundo.go
@@ -15,15 +15,17 @@ func main() {
 	horas := '@'
 
 	fmt.Printf("\nLas \" horas son: %v, %c, %T", horas, horas, horas)
+	/*
 
-	horas = 65
 
-	fmt.Printf("\nLas \" horas son: %c, %.2v, %T", horas, horas, horas)
-	horas = 1235
-	fmt.Printf("\nLas \" horas son: %08.2f, %v, %T", horas, horas, horas)
-	horas = 12365
-	fmt.Printf("\nLas \" horas son: %8.2f, %v, %T\n", horas, horas, horas)
+		horas = 65
 
-	fmt.Println("Las horas son:", horas, " y las horas son", horas)
+		fmt.Printf("\nLas \" horas son: %c, %.2v, %T", horas, horas, horas)
+		horas = 1235
+		fmt.Printf("\nLas \" horas son: %08.2f, %v, %T", horas, horas, horas)
+		horas = 12365
+		fmt.Printf("\nLas \" horas son: %8.2f, %v, %T\n", horas, horas, horas)
 
+		fmt.Println("Las horas son:", horas, " y las horas son", horas)
+	*/
 }
