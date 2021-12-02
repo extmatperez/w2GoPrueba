950833dc437b0176d82b6aba9de3738019a2c2d9
extmatperez <ext_matperez@mercadolibre.com>
2021-12-02 15:35:59 -0300

    Clase 4TM errores personalizados

---
+++ GoWeb/Clase4TM/proyecto/cmd/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/extmatperez/w2GoPrueba/GoWeb/Clase4TM/proyecto/cmd/server/handler"
+	personas "github.com/extmatperez/w2GoPrueba/GoWeb/Clase4TM/proyecto/internal/personas"
+	"github.com/extmatperez/w2GoPrueba/GoWeb/Clase4TM/proyecto/pkg/store"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("No se pudo abrir el archivo .env")
+	}
+
+	router := gin.Default()
+
+	db := store.New(store.FileType, "./personasSalida.json")
+	repo := personas.NewRepository(db)
+	service := personas.NewService(repo)
+	controller := handler.NewPersona(service)
+
+	router.GET("/personas/get", controller.GetAll())
+	router.POST("/personas/add", controller.Store())
+	router.PUT("/personas/:id", controller.Update())
+	router.PATCH("/personas/:id", controller.UpdateNombre())
+	router.DELETE("/personas/:id", controller.Delete())
+
+	router.Run()
+}
